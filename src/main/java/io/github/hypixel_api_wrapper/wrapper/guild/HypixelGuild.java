176aed8cacb9db78f9301658a8514f94c1bcf70e
Sam Jacobson <96966386+unpluggedsam@users.noreply.github.com>
2022-07-08 14:25:28 -0700

    Began HypixelGuild skeleton
    This commit will add methods to the HypixelGuild class that will retrieve information about the guild.

--- src/main/java/io/github/hypixel_api_wrapper/wrapper/guild/HypixelGuild.java
+++ src/main/java/io/github/hypixel_api_wrapper/wrapper/guild/HypixelGuild.java
@@ -1,5 +1,27 @@
 package io.github.hypixel_api_wrapper.wrapper.guild;
 
-public class HypixelGuild {
+import io.github.hypixel_api_wrapper.wrapper.player.HypixelPlayer;
+import java.util.List;
 
+public class HypixelGuild {
+    private String name;
+    private String APIKey;
+
+    public HypixelGuild(String name, String APIKey) {
+        this.name = name;
+        this.APIKey = APIKey;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public List<HypixelPlayer> getMembers() {
+        return null;
+    }
+    public HypixelPlayer getOwner() {
+        return null;
+    }
+    public long getDailyGXP() {
+        return 0;
+    }
 }
