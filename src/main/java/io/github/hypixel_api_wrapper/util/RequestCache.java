d7562fd9602f45f6c0b7e1a24d6d54df2a2c0cd2
InsertIGN <85914720+NotInsertIGN@users.noreply.github.com>
2022-07-09 10:07:47 -0400

    Added #clearCache method to RequestCache.java

--- src/main/java/io/github/hypixel_api_wrapper/util/RequestCache.java
+++ src/main/java/io/github/hypixel_api_wrapper/util/RequestCache.java
@@ -1,17 +1,21 @@
 package io.github.hypixel_api_wrapper.util;
 
 import java.util.HashMap;
+import java.util.Map;
 import org.json.JSONObject;
 
 public class RequestCache {
-    private static HashMap<String, JSONObject> cache = new HashMap<>();
+    private static Map<String, JSONObject> cache = new HashMap<>();
     public static void addRequest(String name, JSONObject obj) {
         cache.put(name, obj);
+    }
+    public static void clearCache() {
+        cache.clear();
     }
     public static JSONObject getFromCache(String name) {
         return cache.get(name);
     }
-    public static HashMap<String, JSONObject> getCache() {
+    public static Map<String, JSONObject> getCache() {
         return cache;
     }
 }
