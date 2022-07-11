ea4c1665a7c1606e16fd7006c6a17d62f43bb0ef
JxBP <61627369+Black-Plasma@users.noreply.github.com>
2022-07-11 20:23:46 +0200

    Fix previous commit

--- src/main/java/io/github/hypixel_api_wrapper/HypixelAPI.java
+++ src/main/java/io/github/hypixel_api_wrapper/HypixelAPI.java
@@ -7,32 +7,36 @@ import io.github.hypixel_api_wrapper.http.RequestFactory;
 import java.io.IOException;
 
 public class HypixelAPI {
+
     private final String key;
+
     private HypixelAPI(String key) {
         this.key = key;
     }
 
     /**
      * Creates a new instance of the HypixelAPI object with caching disabled.
+     *
      * @param key the api key to use for authentication
      * @return the newly created instance
      */
     public static HypixelAPI create(String key) {
-        return create(key, null);
+        return create(key, new NoCachingStrategy());
     }
 
     /**
      * Creates a new instance of the HypixelAPI object
-     * @param key the api key to use for authentication
+     *
+     * @param key             the api key to use for authentication
      * @param cachingStrategy the caching strategy to use. If null is passed the NoCachingStrategy
      *                        will be used to disable caching
      * @return the newly created instance
      */
-    public static HypixelAPI create(String key) {
-        RequestFactory.start(new NoCachingStrategy());
+    public static HypixelAPI create(String key, CachingStrategy cachingStrategy) {
+        RequestFactory.start(cachingStrategy);
         return new HypixelAPI(key);
     }
-    
+
     public static void shutdown() throws IOException {
         RequestFactory.close();
     }
