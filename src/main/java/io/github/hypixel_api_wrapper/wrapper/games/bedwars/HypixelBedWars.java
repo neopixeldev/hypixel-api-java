a3e20b4e9599fba3197898993dbbc8f3d28631c5
Sam Jacobson <96966386+unpluggedsam@users.noreply.github.com>
2022-07-12 14:01:20 -0700

    Deleted instances of subclass from `HypixelBedWars`

--- src/main/java/io/github/hypixel_api_wrapper/wrapper/games/bedwars/HypixelBedWars.java
+++ src/main/java/io/github/hypixel_api_wrapper/wrapper/games/bedwars/HypixelBedWars.java
@@ -4,54 +4,10 @@ import io.github.hypixel_api_wrapper.wrapper.games.HypixelGame;
 import org.json.JSONObject;
 
 public abstract class HypixelBedWars extends HypixelGame {
-
-    HypixelBedWars solos;
-    HypixelBedWars doubles;
-    HypixelBedWars trios;
-    HypixelBedWars squads;
-    HypixelBedWars coreModes;
-    HypixelBedWars fourVSfour;
-    HypixelBedWars rushSolo;
-    HypixelBedWars rushDoubles;
-    HypixelBedWars rushFours;
-    HypixelBedWars ultimateSolo;
-    HypixelBedWars ultimateDoubles;
-    HypixelBedWars ultimateFours;
-    HypixelBedWars luckyDoubles;
-    HypixelBedWars luckyFours;
-    HypixelBedWars castle;
-    HypixelBedWars capture;
-    HypixelBedWars swapDoubles;
-    HypixelBedWars swapFours;
-    HypixelBedWars voidlessFours;
-
     protected HypixelBedWars(JSONObject stats) {
-
         super(stats);
-
-        solos = new HypixelBedWarsSolos(stats);
-        doubles = new HypixelBedWarsDoubles(stats);
-        trios = new HypixelBedWarsTrios(stats);
-        squads = new HypixelBedWarsSquads(stats);
-        coreModes = new HypixelBedWarsCoreModes(stats);
-        fourVSfour = new HypixelBedWars4v4(stats);
-        rushSolo = new HypixelBedWarsRushSolo(stats);
-        rushDoubles = new HypixelBedWarsRushDoubles(stats);
-        rushFours = new HypixelBedWarsRushFours(stats);
-        ultimateSolo = new HypixelBedWarsUltimateSolos(stats);
-        ultimateDoubles = new HypixelBedWarsUltimateDoubles(stats);
-        ultimateFours = new HypixelBedWarsUltimateFours(stats);
-        luckyDoubles = new HypixelBedWarsLuckyDoubles(stats);
-        luckyFours = new HypixelBedWarsLuckyFours(stats);
-        castle = new HypixelBedWarsCastle(stats);
-        capture = new HypixelBedwarsCapture(stats);
-        swapDoubles = new HypixelBedWarsSwapDoubles(stats);
-        swapFours = new HypixelBedWarsSwapFours(stats);
-        voidlessFours = new HypixelBedWarsVoidlessFours(stats);
-
     }
-
-    abstract String dataLocation();
+    abstract String prefix();
     @Override
     public String getGameID() {
         return "BEDWARS";
