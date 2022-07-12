5a8dfc6e31b2c63d53f473653c206e4e7e42391f
Sam Jacobson <96966386+unpluggedsam@users.noreply.github.com>
2022-07-11 18:28:23 -0700

    Added all of the API retrieval methods for HypixelBedWars

--- src/main/java/io/github/hypixel_api_wrapper/wrapper/games/bedwars/HypixelBedWars.java
+++ src/main/java/io/github/hypixel_api_wrapper/wrapper/games/bedwars/HypixelBedWars.java
@@ -66,11 +66,156 @@ public class HypixelBedWars extends HypixelGame {
         return false;
     }
 
-    public int getFKDR() {
+    /**
+     * Methods that should not be overridden by subclasses.
+     */
+
+    public int getCoins() {
         throw new UnsupportedOperationException();
     }
 
-    public int getStar() {
+    public int getLevel() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getCurentWinstreak() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getExperience() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getBoxEpics() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getActiveVictoryDance() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getActiveIslandTopper() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getActiveSprays() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getActiveDeathCry() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getOpenedChestsAmount() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getOpenedCommonChestsAmount() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getOpenedRareChestsAmount() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getOpenedEpicChestsAmount() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getOpenedLegendaryChestsAmount() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getChristmasChestsAmount() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * These methods apply to BedWars overall, but should be overridden by subclasses because they
+     * also apply to each game mode.
+     */
+
+    public int getDiamondsCollected() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getEmeraldsCollected() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getGoldCollected() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getIronCollected() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getKills() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getDeaths() {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getKillToDeathRatio() {
+        return getKills() / getDeaths();
+    }
+
+    public int getFinalKills() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getFinalDeaths() {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getFinalKillToDeathRatio() {
+        return getFinalKills() / getFinalDeaths();
+    }
+
+    public int getWins() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getLosses() {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getWinToLossRatio() {
+        return getWins() / getLosses();
+    }
+
+    public int getBedsBroken() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getVoidKills() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getVoidFinalKills() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getProjectileDeaths() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getProjectileKills() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getItemsPurchased() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getGamesPlayed() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getFireTickKills() {
         throw new UnsupportedOperationException();
     }
 }
