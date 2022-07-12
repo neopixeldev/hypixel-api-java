4d8d11f74eda6f92c536bb3ddaed13e7dddd40c2
Sam Jacobson <96966386+unpluggedsam@users.noreply.github.com>
2022-07-12 15:05:58 -0700

    Refactored HypixelBedWars
    This class will now return all the information about HypixelBedWars overall.
    
    All of the methods to retrieve data about Hypixel Bedwars (Level, Coins, ect,) will also be moved to this class.

--- src/main/java/io/github/hypixel_api_wrapper/wrapper/games/bedwars/HypixelBedWars.java
+++ src/main/java/io/github/hypixel_api_wrapper/wrapper/games/bedwars/HypixelBedWars.java
@@ -1,31 +1,16 @@
 package io.github.hypixel_api_wrapper.wrapper.games.bedwars;
 
-import io.github.hypixel_api_wrapper.wrapper.games.HypixelGame;
 import org.json.JSONObject;
 
-public abstract class HypixelBedWars extends HypixelGame {
+public class HypixelBedWars extends AbstractHypixelBedWars {
+
     protected HypixelBedWars(JSONObject stats) {
         super(stats);
     }
-    abstract String prefix();
     @Override
-    public String getGameID() {
-        return "BEDWARS";
+    String prefix() {
+        return "";
     }
-
-    @Override
-    public String getGameName() {
-        return "Bed Wars";
-    }
-
-    @Override
-    public boolean isRemoved() {
-        return false;
-    }
-
-    /**
-     * Methods that should not be overridden by subclasses.
-     */
 
     public final int getCoins() {
         throw new UnsupportedOperationException();
@@ -82,94 +67,4 @@ public abstract class HypixelBedWars extends HypixelGame {
     public final int getChristmasChestsAmount() {
         throw new UnsupportedOperationException();
     }
-
-    /**
-     * These methods apply to BedWars overall, but should be overridden by subclasses because they
-     * also apply to each game mode.
-     */
-
-    public int getDiamondsCollected() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getEmeraldsCollected() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getGoldCollected() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getIronCollected() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getKills() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getDeaths() {
-        throw new UnsupportedOperationException();
-    }
-
-    public double getKillToDeathRatio() {
-        return getKills() / getDeaths();
-    }
-
-    public int getFinalKills() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getFinalDeaths() {
-        throw new UnsupportedOperationException();
-    }
-
-    public double getFinalKillToDeathRatio() {
-        return getFinalKills() / getFinalDeaths();
-    }
-
-    public int getWins() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getLosses() {
-        throw new UnsupportedOperationException();
-    }
-
-    public double getWinToLossRatio() {
-        return getWins() / getLosses();
-    }
-
-    public int getBedsBroken() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getVoidKills() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getVoidFinalKills() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getProjectileDeaths() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getProjectileKills() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getItemsPurchasedAmount() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getGamesPlayed() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getCurrentWinstreak() {
-        throw new UnsupportedOperationException();
-    }
-
 }
