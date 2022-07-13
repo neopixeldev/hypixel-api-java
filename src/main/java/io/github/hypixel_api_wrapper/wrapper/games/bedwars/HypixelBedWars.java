44ae492ddbdada829b9c04b2cceb5e6e6ec777f5
Sam Jacobson <96966386+unpluggedsam@users.noreply.github.com>
2022-07-13 16:03:59 -0700

    Moved all overall methods to `HypixelBedWars` and created `HypixelBedWarsModes` enum
    This commit will create a `HypixelBedWarsModes` enum where each value has a String relating to it's modes JSON key value. They will be used to get information from the API about a certain mode.

--- src/main/java/io/github/hypixel_api_wrapper/wrapper/games/bedwars/HypixelBedWars.java
+++ src/main/java/io/github/hypixel_api_wrapper/wrapper/games/bedwars/HypixelBedWars.java
@@ -69,4 +69,89 @@ public class HypixelBedWars extends AbstractHypixelBedWars {
         throw new UnsupportedOperationException();
     }
 
+
+    public int getDiamondsCollected() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getEmeraldsCollected() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getGoldCollected() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getIronCollected() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getKills() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getDeaths() {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getKillToDeathRatio() {
+        return getKills() / Math.max(getDeaths(), 1);
+    }
+
+    public int getFinalKills() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getFinalDeaths() {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getFinalKillToDeathRatio() {
+        return getFinalKills() / Math.max(getFinalDeaths(), 1);
+    }
+
+    public int getWins() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getLosses() {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getWinToLossRatio() {
+        return getWins() / Math.max(getLosses(), 1);
+    }
+
+    public int getBedsBroken() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getVoidKills() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getVoidFinalKills() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getProjectileDeaths() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getProjectileKills() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getItemsPurchasedAmount() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getGamesPlayed() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getCurrentWinstreak() {
+        throw new UnsupportedOperationException();
+    }
+
 }
---
+++ src/main/java/io/github/hypixel_api_wrapper/wrapper/games/bedwars/HypixelBedWarsModes.java
@@ -0,0 +1,36 @@
+package io.github.hypixel_api_wrapper.wrapper.games.bedwars;
+
+public enum HypixelBedWarsModes {
+
+    SOLOS("eight_one_"),
+    DOUBLES("eight_two_"),
+    TRIOS("four_three_"),
+    FOURS("four_four_"),
+
+    FOUR_VS_FOUR("two_four_"),
+
+    RUSH_DOUBLES("eight_two_rush_"),
+    RUSH_FOURS("four_four_rush_"),
+
+    ULTIMATE_DOUBLES("eight_two_ultimate_"),
+    ULTIMATE_FOURS("four_four_ultimate_"),
+
+    LUCKY_DOUBLES("eight_two_lucky_"),
+    LUCKY_FOURS("four_four_lucky_"),
+
+    CASTLE("castle_"),
+
+    VOIDLESS_FOURS("four_four_voidless_"),
+
+    OVERALL("");
+
+    String JSONKey;
+
+    HypixelBedWarsModes(String JSONKey) {
+        this.JSONKey = JSONKey;
+    }
+
+    public String getJSONKey() {
+        return JSONKey;
+    }
+}
