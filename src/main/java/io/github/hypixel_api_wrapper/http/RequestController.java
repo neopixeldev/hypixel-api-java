d954a9ce99ef2d96027790bc04e25f214bf45e9c
HillelCS <96966386+unpluggedsam@users.noreply.github.com>
2022-07-23 20:29:11 -0700

    Create constructor for `RequestController`
    This commit will create a constructor for the `RequestController` class. The class will be used to retrieve `Query`'s.

--- src/main/java/io/github/hypixel_api_wrapper/http/RequestController.java
+++ src/main/java/io/github/hypixel_api_wrapper/http/RequestController.java
@@ -1,5 +1,14 @@
 package io.github.hypixel_api_wrapper.http;
 
+import io.github.hypixel_api_wrapper.http.cache.CachingStrategy;
+import java.util.UUID;
+
 public class RequestController {
+
+    private final RequestFactory requestFactory;
+
+    public RequestController(UUID apiKey, CachingStrategy cachingStrategy) {
+        requestFactory = new RequestFactory(apiKey, cachingStrategy);
+    }
 
 }
