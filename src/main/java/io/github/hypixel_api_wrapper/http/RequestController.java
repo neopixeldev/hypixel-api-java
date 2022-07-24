c5947b4022d8a49a5c5ba35bcaeb9bb93849936a
HillelCS <96966386+unpluggedsam@users.noreply.github.com>
2022-07-23 20:40:59 -0700

    Update RequestController.java
    This commit will add a getPlayerByUUID() method to the `RequestController`.

--- src/main/java/io/github/hypixel_api_wrapper/http/RequestController.java
+++ src/main/java/io/github/hypixel_api_wrapper/http/RequestController.java
@@ -1,14 +1,21 @@
 package io.github.hypixel_api_wrapper.http;
 
 import io.github.hypixel_api_wrapper.http.cache.CachingStrategy;
+import io.github.hypixel_api_wrapper.http.query.QueryFactory;
 import java.util.UUID;
+import org.json.JSONObject;
 
 public class RequestController {
 
     private final RequestFactory requestFactory;
+    private final QueryFactory queryFactory = new QueryFactory();
 
     public RequestController(UUID apiKey, CachingStrategy cachingStrategy) {
         requestFactory = new RequestFactory(apiKey, cachingStrategy);
+    }
+
+    public JSONObject getPlayerByUUID(UUID uuid) {
+        return requestFactory.send(queryFactory.getPlayerByUUIDQuery(uuid).createRequest());
     }
 
 }
