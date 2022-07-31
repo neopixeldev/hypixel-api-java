ec969023d9dc7d06c3d9449d7dfbe298e8083bca
Mqlvin <pricklydotcactus@gmail.com>
2022-07-31 23:17:05 +0100

    Add bed destroy enums excluding key

--- src/main/java/io/github/hypixel_api_wrapper/wrapper/games/bedwars/cosmetics/HypixelBedwarsBedDestroys.java
+++ src/main/java/io/github/hypixel_api_wrapper/wrapper/games/bedwars/cosmetics/HypixelBedwarsBedDestroys.java
@@ -4,7 +4,29 @@ import io.github.hypixel_api_wrapper.wrapper.games.HypixelCosmetic;
 import io.github.hypixel_api_wrapper.wrapper.games.HypixelCosmeticRarities;
 
 public enum HypixelBedwarsBedDestroys implements HypixelCosmetic {
-    ;
+    FIREWORK("Firework", "", HypixelCosmeticRarities.COMMON),
+    GHOST("Ghost", "", HypixelCosmeticRarities.COMMON),
+    LIGHTNING_STRIKE("Lightning Strike", "", HypixelCosmeticRarities.COMMON),
+    SQUID_MISSILE("Squid Missile", "", HypixelCosmeticRarities.COMMON),
+
+    GLYPH("Glyph", "", HypixelCosmeticRarities.RARE),
+    PIG_MISSILE("Pig Missile", "", HypixelCosmeticRarities.RARE),
+    LAVA_EXPLOSION("Lava Explosion", "", HypixelCosmeticRarities.RARE),
+
+    BLIZZARD("Blizzard", "", HypixelCosmeticRarities.EPIC),
+    EGGSPLOSION("Eggsplosion", "", HypixelCosmeticRarities.EPIC),
+    THIEF("Thief", "", HypixelCosmeticRarities.EPIC),
+    TORNADO("Tornado", "", HypixelCosmeticRarities.EPIC),
+    PRESENT("Present", "", HypixelCosmeticRarities.EPIC),
+    PUMPKIN_EXPLOSION("Pumpkin Explosion", "", HypixelCosmeticRarities.EPIC),
+
+    BED_BUGS("Bed Bugs", "", HypixelCosmeticRarities.LEGENDARY),
+    BURNED_UP("Burned Up", "", HypixelCosmeticRarities.LEGENDARY),
+    PIGSPLOSION("Pigsplosion", "", HypixelCosmeticRarities.LEGENDARY),
+    LADY_BUG("Lady Bug", "", HypixelCosmeticRarities.LEGENDARY),
+    EGG_POPPER("Egg Popper", "", HypixelCosmeticRarities.LEGENDARY),
+    FISHY("Fishy", "", HypixelCosmeticRarities.LEGENDARY),
+    STORMY("Stormy", "", HypixelCosmeticRarities.LEGENDARY);
 
     private String name;
     private String key;
@@ -18,16 +40,16 @@ public enum HypixelBedwarsBedDestroys implements HypixelCosmetic {
 
     @Override
     public String getFormattedName() {
-        return null;
+        return this.name;
     }
 
     @Override
     public String getKey() {
-        return null;
+        return this.key;
     }
 
     @Override
     public HypixelCosmeticRarities getRarity() {
-        return null;
+        return this.rarity;
     }
 }
