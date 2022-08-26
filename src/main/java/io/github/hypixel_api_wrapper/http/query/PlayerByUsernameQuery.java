9b4588e7be9609ebf47d8d4cf7adecf706604d83
Sam <unpluggedsam990@gmail.com>
2022-08-25 19:00:40 -0700

    Create PlayerByUsernameQuery.java
    This class will be used to retrieve a `Player JSONObject` from a username.

---
+++ src/main/java/io/github/hypixel_api_wrapper/http/query/PlayerByUsernameQuery.java
@@ -0,0 +1,34 @@
+package io.github.hypixel_api_wrapper.http.query;
+
+import io.github.hypixel_api_wrapper.http.Endpoint;
+import okhttp3.HttpUrl;
+
+public class PlayerByUsernameQuery extends Query {
+
+    private final String username;
+
+    protected PlayerByUsernameQuery(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public HttpUrl.Builder createRequest() {
+        return HttpUrl.parse(Endpoint.PLAYER.getURL().toString()).newBuilder()
+            .addQueryParameter("name", username);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        PlayerByUsernameQuery other = (PlayerByUsernameQuery) o;
+
+        if (o == this) {
+            return true;
+        }
+
+        return other.getUsername().equals(username);
+    }
+}
