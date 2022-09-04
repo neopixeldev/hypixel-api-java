29971059083e1c2ac902d7276f22781dbe8e385d
Sam <unpluggedsam990@gmail.com>
2022-09-04 13:29:08 -0700

    Update JSONHandler.java

--- src/main/java/io/github/hypixel_api_wrapper/wrapper/util/JSONHandler.java
+++ src/main/java/io/github/hypixel_api_wrapper/wrapper/util/JSONHandler.java
@@ -1,5 +1,25 @@
 package io.github.hypixel_api_wrapper.wrapper.util;
 
+import java.util.Optional;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class JSONHandler {
 
+    private final JSONObject stats;
+    private String statsPrefix;
+
+
+    public JSONHandler(JSONObject stats) {
+        this.stats = stats;
+    }
+
+    public JSONHandler(JSONObject stats, String statsPrefix) {
+        this.stats = stats;
+        this.statsPrefix = statsPrefix;
+    }
+
+    public <T> Optional<T> getSafe(String key) {
+            return (Optional<T>) Optional.ofNullable(stats.get(statsPrefix + key));
+    }
 }
