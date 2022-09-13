0aa18dd1015c1dbd08835b934599d76511f3cdfb
Sam <96966386+unpluggedsam@users.noreply.github.com>
2022-09-13 03:21:44 -0700

    Exception handling (#84)
    * Refactor PineappleException to NovopixelException
    
    * Refactor RequestValidator to check every possible failure from API
    
    * Added documentation to NovopixelException's
    
    * Update NovopixelException.java
    
    * Rename NovopixelException to NeopixelException
    
    Co-authored-by: Mqlvin <pricklydotcactus@gmail.com>

--- src/main/java/io/github/hypixel_api_wrapper/exception/MissingFieldException.java
+++ src/main/java/io/github/hypixel_api_wrapper/exception/MissingFieldException.java
@@ -3,8 +3,8 @@ package io.github.hypixel_api_wrapper.exception;
 /**
  * Some data is missing, usually a field.
  */
-public class MissingFieldException extends NovopixelException {
 
+public class MissingFieldException extends NeopixelException {
     public MissingFieldException(String message) {
         super(message);
     }
--- src/main/java/io/github/hypixel_api_wrapper/exception/UnknownAPIException.java
+++ src/main/java/io/github/hypixel_api_wrapper/exception/UnknownAPIException.java
@@ -3,8 +3,8 @@ package io.github.hypixel_api_wrapper.exception;
 /**
  * Other exceptions from the API that have not been handled.
  */
-public class UnknownAPIException extends NovopixelException {
 
+public class UnknownAPIException extends NeopixelException {
     public UnknownAPIException(String message) {
         super(message);
     }
