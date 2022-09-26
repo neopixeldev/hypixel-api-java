c4cb3c5d42aede64a231fd9893039176cfa104f4
Sam <unpluggedsam990@gmail.com>
2022-09-25 17:57:28 -0700

    Create HypixelGuildRank.java
    This class will be used to keep track of a `HypixelGuild`'s ranks.

---
+++ src/main/java/io/github/neopixel/wrapper/guild/HypixelGuildRank.java
@@ -0,0 +1,33 @@
+package io.github.neopixel.wrapper.guild;
+
+import java.time.Instant;
+import org.json.JSONObject;
+
+public class HypixelGuildRank {
+
+    public final JSONObject object;
+
+    public HypixelGuildRank(JSONObject object) {
+        this.object = object;
+    }
+
+    public String getName() {
+        return object.getString("name");
+    }
+
+    public boolean isDefault() {
+        return object.getBoolean("default");
+    }
+
+    public String getTag() {
+        return object.getString("tag");
+    }
+
+    public Instant getDateCreated() {
+        return Instant.ofEpochSecond(object.getLong("created"));
+    }
+
+    public int getPriority() {
+        return object.getInt("priority");
+    }
+}
