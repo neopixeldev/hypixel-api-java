81c4915d8d0ba2ffbf2ff8b3acc40d25b8a188c1
Sam <unpluggedsam990@gmail.com>
2022-09-28 18:21:47 -0700

    Create HypixelGameTypes.java

---
+++ src/main/java/io/github/neopixel/wrapper/util/HypixelGameTypes.java
@@ -0,0 +1,34 @@
+package io.github.neopixel.wrapper.util;
+
+public enum HypixelGameTypes {
+
+    REPLAY,
+    ARENA,
+    QUAKECRAFT,
+    PROTOTYPE,
+    TNTGAMES,
+    WOOL_GAMES,
+    SKYWARS,
+    SURVIVAL_GAMES,
+    WALLS,
+    BUILD_BATTLE,
+    PAINTBALL,
+    MCGO,
+    SUPER_SMASH,
+    SPEED_UHC,
+    GINGERBREAD,
+    VAMPIREZ,
+    BEDWARS,
+    BATTLEGROUND,
+    ARCADE,
+    DUELS,
+    SMP,
+    UHC,
+    WALLS3,
+    PIT,
+    SKYBLOCK,
+    MURDER_MYSTERY,
+    LEGACY,
+    HOUSING;
+    
+}
