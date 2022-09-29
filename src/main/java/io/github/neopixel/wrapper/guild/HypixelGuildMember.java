7254a98f6c02d743d7e9247cfb14863bae87c1bb
Sam <unpluggedsam990@gmail.com>
2022-09-28 18:14:00 -0700

    Add methods to HypixelGuildMember

--- src/main/java/io/github/neopixel/wrapper/guild/HypixelGuildMember.java
+++ src/main/java/io/github/neopixel/wrapper/guild/HypixelGuildMember.java
@@ -2,49 +2,85 @@ package io.github.neopixel.wrapper.guild;
 
 import io.github.neopixel.http.RequestController;
 import io.github.neopixel.wrapper.player.HypixelPlayer;
+import io.github.neopixel.wrapper.util.JSONHandler;
 import io.github.neopixel.wrapper.util.UnformattedStringToUUID;
 import java.time.Instant;
-import java.util.Optional;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.json.JSONObject;
 
 /**
- * Used to retrieve statistics about a {@link io.github.neopixel.wrapper.player.HypixelPlayer HypixelPlayer's} guild statistics.
+ * Used to retrieve statistics about a
+ * {@link io.github.neopixel.wrapper.player.HypixelPlayer HypixelPlayer's} guild statistics.
  */
 public class HypixelGuildMember {
 
-    private final JSONObject guildMemberObject;
+    private final JSONHandler jsonHandler;
     private final HypixelGuild guild;
     private final RequestController requestController;
 
-    public HypixelGuildMember(JSONObject guildMemberObject, HypixelGuild guild, RequestController requestController) {
-        this.guildMemberObject = guildMemberObject;
+    public HypixelGuildMember(JSONHandler jsonHandler, HypixelGuild guild,
+        RequestController requestController) {
+        this.jsonHandler = jsonHandler;
         this.guild = guild;
         this.requestController = requestController;
     }
 
-    public Optional<HypixelGuildRank> getRank() {
-        return guild.getGuildRanks().get().stream().filter(rank -> rank.getName().equals(guildMemberObject.getString("rank"))).findFirst();
+    public HypixelGuildRank getRank() {
+        return guild.getGuildRanks().stream()
+            .filter(rank -> rank.getName().equals(jsonHandler.getSafeString("rank"))).findFirst()
+            .get();
     }
 
     public int getQuestParticipation() {
-        return guildMemberObject.getInt("questParticipation");
+        return jsonHandler.getSafeInt("questParticipation");
     }
 
     public Instant getDateJoined() {
-        return Instant.ofEpochSecond(guildMemberObject.getLong("joined"));
+        return Instant.ofEpochSecond(jsonHandler.getSafeLong("joined"));
     }
 
     public UUID getUUID() {
-        return UnformattedStringToUUID.convertUnformattedStringToUUID(guildMemberObject.getString("uuid"));
+        return UnformattedStringToUUID.convertUnformattedStringToUUID(
+            jsonHandler.getSafeString("uuid"));
     }
 
-    public Instant getDailyGXP() {
-        throw new UnsupportedOperationException();
+    public int getDailyGXP() {
+        String dailyGXPObject = jsonHandler.getJSONHandler("expHistory").getKeys().next();
+        return jsonHandler.getJSONHandler("expHistory").getSafeInt(dailyGXPObject);
     }
 
-    public Instant getWeeklyGXP() {
-        throw new UnsupportedOperationException();
+    public int getWeeklyGXPCombined() {
+
+        AtomicInteger weeklyGXP = new AtomicInteger();
+
+        jsonHandler.getJSONHandler("expHistory").getKeys().forEachRemaining(date -> {
+            weeklyGXP.addAndGet(jsonHandler.getJSONHandler("expHistory").getSafeInt(date));
+        });
+
+        return weeklyGXP.get();
+    }
+
+    public Map<Instant, Integer> getWeeklyGXPByDay() {
+
+        Map<Instant, Integer> weeklyGXPByDateMap = new HashMap<>();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("uuuu-mm-dd");
+
+        jsonHandler.getJSONHandler("expHistory").getKeys().forEachRemaining(date -> {
+            ZonedDateTime zonedDateTime = ZonedDateTime.parse(date, dateTimeFormatter);
+            weeklyGXPByDateMap.put(zonedDateTime.toInstant(),
+                jsonHandler.getJSONHandler("expHistory").getSafeInt(date));
+        });
+
+        return weeklyGXPByDateMap;
+    }
+
+    public int getMutedTill() {
+        return jsonHandler.getSafeInt("mutedTill");
     }
 
     public HypixelPlayer asPlayer() {
