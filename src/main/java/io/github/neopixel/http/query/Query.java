a51449c675701f8f32175b9d6c850928198d90e2
Sam <unpluggedsam990@gmail.com>
2022-10-04 10:40:50 -0700

    Add supresswarning

--- src/main/java/io/github/neopixel/http/query/Query.java
+++ src/main/java/io/github/neopixel/http/query/Query.java
@@ -14,6 +14,7 @@ import okhttp3.Request;
  */
 public class Query {
 
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     private final Optional<QueryParameter[]> queryParameterList;
     private final Endpoint endpoint;
 
