febba75d01897c9b09ea91d9d287f2bba54a4ea5
Sam <unpluggedsam990@gmail.com>
2022-10-23 17:28:29 -0700

    Implement HypixelUHC

--- src/main/java/io/github/neopixel/wrapper/games/uhc/HypixelUHC.java
+++ src/main/java/io/github/neopixel/wrapper/games/uhc/HypixelUHC.java
@@ -23,4 +23,45 @@ public class HypixelUHC extends HypixelGame {
     public boolean isRemoved() {
         return false;
     }
+
+    public int getCoins() {
+        return jsonHandler.getSafeInt("coins");
+    }
+
+    public boolean isTeammateDamage() {
+        return jsonHandler.getSafeBoolean("teammate_damage");
+    }
+
+    public HypixelUHCBrawl getBrawlStats() {
+        return new HypixelUHCBrawl(jsonHandler);
+    }
+
+    public HypixelUHCSoloBrawl getBrawlSolosStats() {
+        return new HypixelUHCSoloBrawl(jsonHandler);
+    }
+
+    public HypixelUHCDuoBrawl getBrawlDoublesStats() {
+        return new HypixelUHCDuoBrawl(jsonHandler);
+    }
+
+    public HypixelUHCNoDiamonds getNoDiamondsStats() {
+        return new HypixelUHCNoDiamonds(jsonHandler);
+    }
+
+    public HypixelUHCRedVSBlue getRedVSBlueStats() {
+        return new HypixelUHCRedVSBlue(jsonHandler);
+    }
+
+    public HypixelUHCSolos getSolosStats() {
+        return new HypixelUHCSolos(jsonHandler);
+    }
+
+    public HypixelUHCTeams getTeamsStats() {
+        return new HypixelUHCTeams(jsonHandler);
+    }
+
+    public HypixelUHCVanillaDoubles getVanillaDoublesStats() {
+        return new HypixelUHCVanillaDoubles(jsonHandler);
+    }
+
 }
