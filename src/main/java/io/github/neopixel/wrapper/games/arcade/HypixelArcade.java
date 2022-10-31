68ef54d675201e8212321d9693423e43181ee730
Mqlvin <pricklydotcactus@gmail.com>
2022-10-31 16:54:30 +0000

    Remove unknown methods

--- src/main/java/io/github/neopixel/wrapper/games/arcade/HypixelArcade.java
+++ src/main/java/io/github/neopixel/wrapper/games/arcade/HypixelArcade.java
@@ -1,8 +1,6 @@
 package io.github.neopixel.wrapper.games.arcade;
 
 import io.github.neopixel.wrapper.games.HypixelGame;
-import io.github.neopixel.wrapper.games.housing.HypixelHousing;
-import io.github.neopixel.wrapper.player.HypixelFriend;
 import io.github.neopixel.wrapper.util.JSONHandler;
 import java.time.Instant;
 
@@ -31,14 +29,6 @@ public class HypixelArcade extends HypixelGame {
         return jsonHandler.getSafeInt("coins");
     }
 
-    public boolean isHintsEnabled() {
-        return jsonHandler.getSafeBoolean("hints");
-    }
-
-    public boolean isMusicEnabled() {
-        return jsonHandler.getSafeBoolean("music");
-    }
-
     public int getWeeklyCoinsA() {
         return jsonHandler.getSafeInt("coins_weekly_a");
     }
@@ -54,15 +44,7 @@ public class HypixelArcade extends HypixelGame {
     public int getMonthlyCoinsB() {
         return jsonHandler.getSafeInt("monthly_coins_b");
     }
-
-    public int getStampLevel() {
-        return jsonHandler.getSafeInt("stamp_level");
-    }
-
-    public int getPoopCollected() {
-        return jsonHandler.getSafeInt("poop_collected");
-    }
-
+    
     public Instant getLastTourneyAd() {
         return Instant.ofEpochSecond(jsonHandler.getSafeLong("lastTourneyAd"));
     }
@@ -71,29 +53,20 @@ public class HypixelArcade extends HypixelGame {
         return jsonHandler.getSafeBoolean("showinfobook");
     }
 
-    public int getMaxWave() {
-        return jsonHandler.getSafeInt("max_wave");
-    }
-
-
-    public HypixelHitw getHitwStats() {
-        return new HypixelHitw(jsonHandler);
-    }
-
     public HypixelPixelParty getPixelPartyStats() {
         return new HypixelPixelParty(jsonHandler.getJSONHandler("pixel_party"));
     }
 
-    public HypixelArcadeSW getArcadeSWStats() {
-        return new HypixelArcadeSW(jsonHandler);
+    public HypixelGalaxyWars getArcadeSWStats() {
+        return new HypixelGalaxyWars(jsonHandler);
     }
 
-    public HypixelOneInTheQuiver getOneInTheQuiverStats() {
-        return new HypixelOneInTheQuiver(jsonHandler);
+    public HypixelBountyHunters getOneInTheQuiverStats() {
+        return new HypixelBountyHunters(jsonHandler);
     }
 
-    public HypixelDayOne getDayOneStats() {
-        return new HypixelDayOne(jsonHandler);
+    public HypixelBlockingDead getDayOneStats() {
+        return new HypixelBlockingDead(jsonHandler);
     }
 
     public HypixelZombies getZombiesStats() {
@@ -124,16 +97,12 @@ public class HypixelArcade extends HypixelGame {
         return new HypixelEnderSpleef(jsonHandler);
     }
 
-    public HypixelDragonWars2 getDragonWars2Stats() {
-        return new HypixelDragonWars2(jsonHandler);
+    public HypixelDragonWars getDragonWars2Stats() {
+        return new HypixelDragonWars(jsonHandler);
     }
 
     public HypixelHideAndSeek getHideAndSeekStats() {
         return new HypixelHideAndSeek(jsonHandler);
-    }
-
-    public HypixelEnder getEnderStats() {
-        return new HypixelEnder(jsonHandler);
     }
 
     public HypixelThrowout getThrowoutStats() {
