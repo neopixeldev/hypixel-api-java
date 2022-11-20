10826b2a69bf6422efb326dc4bb2f0b838a598fd
Nopock <78834703+Nopock@users.noreply.github.com>
2022-11-19 23:42:15 -0800

    Updated `QueryParamter.java` to java 8

--- src/main/java/io/github/neopixel/http/query/QueryParameter.java
+++ src/main/java/io/github/neopixel/http/query/QueryParameter.java
@@ -1,12 +1,20 @@
 package io.github.neopixel.http.query;
 
-/**
- * Keeps track of the parameters passed into the {@link Query} constructor.
- *
- * @param type  The type of the parameter being passed in.
- * @param value The value of the parameter being passed in.
- */
-public record QueryParameter(HypixelQueryParameterTypes type, String value) {
+public class QueryParameter {
+
+    private final HypixelQueryParameterTypes type;
+    private final String value;
+
+    /**
+     * Keeps track of the parameters passed into the {@link Query} constructor.
+     *
+     * @param type  The type of the parameter being passed in.
+     * @param value The value of the parameter being passed in.
+     */
+    public QueryParameter(HypixelQueryParameterTypes type, String value) {
+        this.type = type;
+        this.value = value;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -17,5 +25,13 @@ public record QueryParameter(HypixelQueryParameterTypes type, String value) {
 
         QueryParameter other = (QueryParameter) o;
         return type.equals(other.type()) && value.equals(other.value());
+    }
+
+    public HypixelQueryParameterTypes type() {
+        return type;
+    }
+
+    public String value() {
+        return value;
     }
 }
